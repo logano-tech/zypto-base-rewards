921044c1694b540d4fd1a4a4f6e499f93aba9372
logano-tech <looga33@gmail.com>
2026-02-25 11:04:53 +0200

    Update route.ts

--- src/app/api/rewards/route.ts
+++ src/app/api/rewards/route.ts
@@ -1,1 +1,17 @@
+import { NextResponse } from 'next/server';
 
+export async function POST() {
+  return new NextResponse(
+    `<!DOCTYPE html>
+    <html>
+      <head>
+        <meta property="fc:frame" content="vNext" />
+        <meta property="fc:frame:image" content="https://zypto-base-rewards.vercel.app/images/image_3d84a5.jpg" />
+        <meta property="fc:frame:button:1" content="Download Zypto App" />
+        <meta property="fc:frame:button:1:action" content="link" />
+        <meta property="fc:frame:button:1:target" content="https://ref.zypto.com/VMvrJEHIvPb" />
+      </head>
+    </html>`,
+    { status: 200, headers: { 'Content-Type': 'text/html' } }
+  );
+}
